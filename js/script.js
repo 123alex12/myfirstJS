72e535decebcf302b98968d7fcccbeba3fcf0b3c
Tim <pink98855@gmail.com>
2022-01-10 16:16:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,129 @@
+"use strict";    
+
+// a = 15;
+// console.log(a);
+
+
+
+// let number = 5;
+// let Number = 5;
+// const leftborderwidth = 1;
+
+// number=10;
+// console.log(number);
+
+// const obj = {
+//     a:50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+
+// var name ='Ivan';
+// console.log(name); 
+ 
+// {
+//     var result = 50;
+// }
+// console.log(result);
+
+// alert(5);
+// [].push('a');
+
+
+// let number = 4.6;
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+// const bool = true; 
+
+
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj["name"]);
+// let arr = ['plum.png','orange.jpg',6,'apple.bmp',{},[]];
+// console.log(arr[1]);
+
+// alert('Hello');
+//  const result = confirm("Are you here?"); 
+//  console.log(result); 
+
+// const answer = +prompt("Вам есть 18?","18");
+// console.log(answer + 10);
+
+// const answers = [];
+// answers[0]=prompt('What is your name?','');
+// answers[1]=prompt('What is your surname?','');
+// answers[2]=prompt('How old are you?','');
+
+// // document.write(answers);    
+// console.log(typeof(answers));
+ 
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+
+console.log('arr' + "- object");
+console.log( 4 + + "5");
+ let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+// && 
+// ||
+
+const isChecked = false,
+    isClose = false; 
+
+console.log(isChecked || !isClose);    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
